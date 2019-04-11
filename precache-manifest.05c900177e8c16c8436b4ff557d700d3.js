c6ce3e0d427ee62d06446f1c5734270a282c291f
yeonggwan <hjk781@naver.com>
2019-04-11 10:59:28 +0900

    Updates

---
+++ precache-manifest.05c900177e8c16c8436b4ff557d700d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3740d107e1f39c5454ac",
+    "url": "/movie.app/static/css/main.0f672609.chunk.css"
+  },
+  {
+    "revision": "3740d107e1f39c5454ac",
+    "url": "/movie.app/static/js/main.0d2cf361.chunk.js"
+  },
+  {
+    "revision": "ece4dfe1cec154afa8de",
+    "url": "/movie.app/static/js/runtime~main.d9fec8bc.js"
+  },
+  {
+    "revision": "74cd83497257c4285840",
+    "url": "/movie.app/static/js/2.4d24fb29.chunk.js"
+  },
+  {
+    "revision": "37fb6e540812940fd0e1468f424b1799",
+    "url": "/movie.app/index.html"
+  }
+];
